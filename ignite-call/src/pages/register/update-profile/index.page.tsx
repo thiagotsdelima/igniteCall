8b265b39e1327a45fdfa89f5d7cb542434636b22
thiagotsdelima <thiagotsdelima@outlook.com>
2024-05-30 22:34:13 -0300

    created update-profile
    created update-profile

---
+++ ignite-call/src/pages/register/update-profile/index.page.tsx
@@ -0,0 +1,86 @@
+import {
+  Avatar,
+  Button,
+  Heading,
+  MultiStep,
+  Text,
+  TextArea,
+} from '@ignite-ui/react'
+import { ArrowArcRight } from 'phosphor-react'
+import { useForm } from 'react-hook-form'
+import { z } from 'zod'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { Container, Header } from '../styles'
+import { FormAnnotation, ProfileBox } from './styles'
+import { useSession } from 'next-auth/react'
+import { GetServerSideProps } from 'next'
+import { getServerSession } from 'next-auth'
+import { buildNextAuthOptions } from '@/pages/api/auth/[...nextauth].api'
+
+const updateProfileSchema = z.object({
+  bio: z.string(),
+})
+
+type UpdateProfileData = z.infer<typeof updateProfileSchema>
+
+export default function UpdateProfile() {
+  const {
+    register,
+    handleSubmit,
+    formState: { isSubmitting },
+  } = useForm<UpdateProfileData>({
+    resolver: zodResolver(updateProfileSchema),
+  })
+  const session = useSession()
+  console.log(session)
+
+  async function handleUpdateProfile(data: UpdateProfileData) {}
+
+  return (
+    <Container>
+      <Header>
+        <Heading as="strong">welcome of Ignite Call!</Heading>
+        <Text>
+          Precisamos de algumas informações para criar seu perfil! Ah, você pode
+          editar essas informações depois.
+        </Text>
+        <MultiStep size={4} currentStep={1} />
+      </Header>
+      <ProfileBox as="form" onSubmit={handleSubmit(handleUpdateProfile)}>
+        <label>
+          <Text size="sm">photo of profile</Text>
+          <Avatar
+            src={session.data?.user.avatar_url}
+            alt={session.data?.user.name}
+          />
+        </label>
+        <label>
+          <Text size="sm">about you</Text>
+          <TextArea {...register('bio')} />
+          <FormAnnotation size="sm">
+            Tell me a little about you. This will be displayed on your personal
+            page.
+          </FormAnnotation>
+        </label>
+        <Button type="submit" disabled={isSubmitting}>
+          Finished
+          <ArrowArcRight />
+        </Button>
+      </ProfileBox>
+    </Container>
+  )
+}
+
+export const getServerSideProps: GetServerSideProps = async ({ req, res }) => {
+  const session = await getServerSession(
+    req,
+    res,
+    buildNextAuthOptions(req, res),
+  )
+
+  return {
+    props: {
+      session,
+    },
+  }
+}
