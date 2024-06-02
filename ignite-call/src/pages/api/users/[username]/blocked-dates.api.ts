10fc4da30c6c4b84e54669d13275f0b937f5ef52
thiagotsdelima <thiagotsdelima@outlook.com>
2024-06-02 11:26:08 -0300

    adjustment DB
    adjustment DB

--- ignite-call/src/pages/api/users/[username]/blocked-dates.api.ts
+++ ignite-call/src/pages/api/users/[username]/blocked-dates.api.ts
@@ -44,11 +44,22 @@ export default async function handle(
 
   // reotrna os dias que estao disabilitados
   const blockedDatesRaw = await prisma.$queryRaw`
-    SELECT * FROM schedulings S
+    SELECT 
+      EXTRACT(DAY FROM S.date) AS date,
+      COUNT(S.date) AS amount,
+     ((UTI.time_end_in_minutes - UTI.time_start_in_minutes) / 60) AS size
+
+    FROM schedulings S
+
+    LEFT JOIN user_time_interval UTI
+      ON UTI.week_day = WEEKDAY(DATE_ADD(S.date, INTERVAL 1 DAY))
 
     WHERE S.user_id = ${user.id}
       AND DATE_FORMAT(S.date, "%Y-%m") = ${`${year}-${month}`}
-  `
 
-  return res.json({ blockedWeekDays, blockedDatesRaw })
+      GROUP BY EXTRACT(DAY FROM S.date),
+      ((UTI.time_end_in_minutes - UTI.time_start_in_minutes) / 60)
+  `
+  console.log(blockedDatesRaw)
+  return res.json({ blockedWeekDays })
 }
